8bf5194986fa6fd2f93a36aed1111bdfedddfd03
Elf Sundae <elf.sundae@gmail.com>
2016-04-03 03:46:04 +0800

    ACAuthVerifyPhoneViewController

---
+++ AppComponents/Auth/ACAuthVerifyPhoneViewController+Subclassing.m
@@ -0,0 +1,113 @@
+//
+//  ACAuthVerifyPhoneViewController+Action.m
+//  AppComponents
+//
+//  Created by Elf Sundae on 16/1/22.
+//  Copyright © 2016年 www.0x123.com. All rights reserved.
+//
+
+#import "ACAuthVerifyPhoneViewController.h"
+
+@implementation ACAuthVerifyPhoneViewController (Action)
+
+- (void)confirmSendCodeToPhone:(NSString *)phoneNumber codeType:(ACAuthVerifyPhoneCodeType)codeType fromSecondaryButton:(BOOL)fromSecondaryButton
+{
+        if (!ESIsStringWithAnyText(phoneNumber)) return;
+        
+        NSString *askTitle = nil;
+        NSString *askMessage = nil;
+        NSString *askOKTitle = nil;
+        NSString *askCancelTitle = @"取消";
+        if (ACAuthVerifyPhoneCodeTypeSMS == codeType && !fromSecondaryButton) {
+                askTitle = NSStringWith(@"我们将发送验证码短信到手机：\n%@", phoneNumber);
+                askOKTitle = @"好";
+        } else if (ACAuthVerifyPhoneCodeTypeSMS == codeType && fromSecondaryButton) {
+                askTitle = @"没有接听到来电？\n\n我们可以发送验证码短信到您的手机。";
+                askOKTitle = @"接收验证码短信";
+        } else if (ACAuthVerifyPhoneCodeTypePhoneCall == codeType && !fromSecondaryButton) {
+                askTitle = NSStringWith(@"我们将致电：\n%@\n语音播报验证码，请接听来电", phoneNumber);
+                askOKTitle = @"好";
+        } else if (ACAuthVerifyPhoneCodeTypePhoneCall == codeType && fromSecondaryButton) {
+                askTitle = @"收不到验证码短信？\n\n我们可以致电您的手机，语音播报验证码。如不希望被来电打扰请继续等待短信验证码。";
+                askOKTitle = @"接听语音验证码";
+        }
+        if (!askTitle || !askOKTitle) {
+                return;
+        }
+        
+        ESWeakSelf;
+        // 第一次询问时用cancelButtonTitle，以明显确认按钮
+        UIAlertView *alert = [UIAlertView alertViewWithTitle:askTitle message:askMessage cancelButtonTitle:(fromSecondaryButton ? askCancelTitle : askOKTitle) didDismissBlock:^(UIAlertView *alertView, NSInteger buttonIndex) {
+                ESStrongSelf;
+                if (!fromSecondaryButton && buttonIndex == alertView.cancelButtonIndex) {
+                        [[_self class] setSharedDateOfPreviousSendingCode:[NSDate date]];
+                        [_self startTimerIfNeeded];
+                        [_self sendCodeToPhone:phoneNumber codeType:codeType];
+                } else if (fromSecondaryButton && buttonIndex != alertView.cancelButtonIndex) {
+                        [_self sendSecondaryCodeToPhone:phoneNumber codeType:codeType];
+                }
+        } otherButtonTitles:(fromSecondaryButton ? askOKTitle : askCancelTitle), nil];
+        [alert show];
+}
+
+- (void)sendCodeToPhone:(NSString *)phoneNumber codeType:(ACAuthVerifyPhoneCodeType)codeType
+{
+}
+
+- (void)sendSecondaryCodeToPhone:(NSString *)phoneNumber codeType:(ACAuthVerifyPhoneCodeType)codeType
+{
+}
+
+- (void)verifyPhone:(NSString *)phone phoneZone:(NSString *)phoneZone code:(NSString *)code
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - Action Handler
+
+- (void)sendCodeButtonAction:(id)sender
+{
+        BOOL fromSecondarySendButton = (sender == self.sendSecondaryCodeButton);
+        NSString *phoneNumber = nil;
+        
+        if (!fromSecondarySendButton && ![self canSendCode:&phoneNumber timeRemains:NULL]) {
+                return;
+        } else if (fromSecondarySendButton && !(phoneNumber = [[self class] validatePhoneNumber:self.phoneTextField.text])) {
+                return;
+        }
+        
+        ACAuthVerifyPhoneCodeType codeType = ACAuthVerifyPhoneCodeTypeSMS;
+        if (!fromSecondarySendButton) {
+                codeType = (ACAuthVerifyPhoneCodeType)[self.supportedCodeTypes.firstObject integerValue];
+        } else if (self.supportedCodeTypes.count > 1) {
+                codeType = (ACAuthVerifyPhoneCodeType)[self.supportedCodeTypes[1] integerValue];
+        } else {
+                return;
+        }
+        
+        [self confirmSendCodeToPhone:phoneNumber codeType:codeType fromSecondaryButton:fromSecondarySendButton];
+}
+
+- (void)commitButtonAction:(id)sender
+{
+        NSString *phone = /*[[self class] sharedPhoneNumber] ?: */[[self class] validatePhoneNumber:self.phoneTextField.text];
+        if (!phone) return;
+        
+        NSString *phoneZone = [[self class] sharedPhoneZone] ?: @"86";
+        
+        NSString *code = self.codeTextFiled.text.trim;
+        if (![code isKindOfClass:[NSString class]] || code.length < 4) {
+                return;
+        }
+        
+        if (self.verifyHandler) {
+                self.verifyHandler(self, @{ @"phone": phone,
+                                            @"zone": phoneZone,
+                                            @"code": code });
+        } else {
+                [self verifyPhone:phone phoneZone:phoneZone code:code];
+        }
+}
+
+@end
--- AppComponents/Auth/ACAuthVerifyPhoneViewController.h
+++ AppComponents/Auth/ACAuthVerifyPhoneViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <AppComponents/ACAuthBaseViewController.h>
+#import <ESFramework/ESFrameworkCore.h>
 #import <ESFramework/ESButton.h>
 @class ACAuthVerifyPhoneViewController;
 
@@ -26,10 +27,6 @@ typedef NS_ENUM(NSInteger, ACAuthVerifyPhoneCodeType) {
 
 @property (nonatomic, copy) void (^verifyHandler)(ACAuthVerifyPhoneViewController *controller, NSDictionary *data);
 
-/**
- * 短信签名
- */
-@property (nonatomic, copy) NSString *MobSMSSignature;
 /**
  * 默认为@"验证手机号"
  */
@@ -100,5 +97,76 @@ typedef NS_ENUM(NSInteger, ACAuthVerifyPhoneCodeType) {
 
 + (NSDate *)sharedDateOfPreviousSendingCode;
 + (void)setSharedDateOfPreviousSendingCode:(NSDate *)date;
+
+@end
+
+@interface ACAuthVerifyPhoneViewController () <UITextFieldDelegate>
+/**
+ * 是否可以显示发送第二验证码的按钮。
+ * 在发送过第二验证码后请设置该值为NO， 第二验证码只能发送一次
+ */
+@property (nonatomic) BOOL canShowSendSecondaryCodeButton;
+
+/// 从notification.object中获取回调的textFiled
+- (void)textFieldTextDidChangeNotification:(NSNotification *)notification;
+
+@end
+
+@interface ACAuthVerifyPhoneViewController (Private)
+
+/**
+ * 设置textField右边button的 title 和 enable，并自动跟textField对齐. title为nil时隐藏textField的rightView
+ */
+- (void)setTextFieldRightButtonTitle:(ESButton *)button title:(NSString *)title enabled:(BOOL)enabled;
+
+/**
+ * 当前禁止发送的剩余时间（秒）， 如果为0或者负数则可以发送验证码
+ */
+- (NSTimeInterval)currentTimeRemainsToEnableSendingCode;
+/**
+ * 对currentTimeRemainsToEnableSendingCode进行ceil操作
+ */
+- (int)currentTimeRemainsToEnableSendingCodeAsInt;
+
+/**
+ * 当前是否可以发送验证码短信. 可能是没填手机号，可能是当前禁止重新发送.
+ *
+ * @param phoneNumber 当前有效的手机号
+ * @param timeRemains 还需要多少时间才可以重新发送
+ */
+- (BOOL)canSendCode:(NSString **)phoneNumber timeRemains:(int *)timeRemains;
+
+/**
+ * 重新发送验证码的倒计时. timer间隔1秒调用 -updateUI 方法
+ */
+- (void)startTimerIfNeeded;
+- (void)stopTimer;
+@end
+
+@interface ACAuthVerifyPhoneViewController (Subclassing)
+
+/**
+ * 谈提示框，确认是否发送验证码
+ */
+- (void)confirmSendCodeToPhone:(NSString *)phoneNumber codeType:(ACAuthVerifyPhoneCodeType)codeType fromSecondaryButton:(BOOL)fromSecondaryButton;
+/**
+ * 发送主要的验证码方式
+ * @see supportedCodeTypes
+ */
+- (void)sendCodeToPhone:(NSString *)phoneNumber codeType:(ACAuthVerifyPhoneCodeType)codeType;
+/**
+ * 发送第二验证码
+ * @see supportedCodeTypes
+ */
+- (void)sendSecondaryCodeToPhone:(NSString *)phoneNumber codeType:(ACAuthVerifyPhoneCodeType)codeType;
+/**
+ * 验证手机和验证码是否正确.
+ * 如果self.verifyHandler没有被设置，就调用这个方法.
+ */
+- (void)verifyPhone:(NSString *)phone phoneZone:(NSString *)phoneZone code:(NSString *)code;
+
+
+- (void)sendCodeButtonAction:(id)sender;
+- (void)commitButtonAction:(id)sender;
 
 @end
