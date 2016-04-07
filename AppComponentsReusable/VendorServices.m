32a736b2609ee32ea0147d88f5eee85d42405def
Elf Sundae <elf.sundae@gmail.com>
2016-04-07 19:23:31 +0800

    VendorServices: Add -showShareWithData:shareStateChanged:

--- AppComponentsReusable/VendorServices.m
+++ AppComponentsReusable/VendorServices.m
@@ -36,6 +36,21 @@
 #import <APIService-XGPush/XGPush.h>
 #endif
 
+// ShareSDK: http://mob.com
+// pod 'MOBFoundation_IDFA'
+// pod 'ShareSDK3'
+// pod 'ShareSDK3/ShareSDKUI'
+// pod 'ShareSDK3/ShareSDKPlatforms/WeChat'
+// pod 'ShareSDK3/ShareSDKPlatforms/QQ'
+#if defined(kShareSDKAppKey)
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKUI/ShareSDKUI.h>
+#import <ShareSDK3/WXApi.h>
+#import <ShareSDKConnector/ShareSDKConnector.h>
+#import <TencentOpenAPI/TencentOAuth.h>
+#import <TencentOpenAPI/QQApiInterface.h>
+#endif
+
 @implementation ESApp (VendorServices)
 
 - (void)setupVendorServices
@@ -73,6 +88,39 @@
         [XGPush startApp:kXGPushAppID appKey:kXGPushAppKey];
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(ESAppDidReceiveRemoteNotificationNotification_XGPushHandler:) name:ESAppDidReceiveRemoteNotificationNotification object:nil];
 #endif
+        
+#if defined(kShareSDKAppKey)
+        [ShareSDK registerApp:kShareSDKAppKey
+              activePlatforms:@[@(SSDKPlatformTypeWechat),
+                                @(SSDKPlatformTypeQQ),
+                                @(SSDKPlatformTypeSMS),
+                                @(SSDKPlatformTypeCopy),
+                                @(SSDKPlatformTypeMail)]
+                     onImport:^(SSDKPlatformType platformType)
+         {
+                 switch (platformType) {
+                         case SSDKPlatformTypeWechat:
+                                 [ShareSDKConnector connectWeChat:[WXApi class]];
+                                 break;
+                         case SSDKPlatformTypeQQ:
+                                 [ShareSDKConnector connectQQ:[QQApiInterface class] tencentOAuthClass:[TencentOAuth class]];
+                                 break;
+                         default:
+                                 break;
+                 }
+         } onConfiguration:^(SSDKPlatformType platformType, NSMutableDictionary *appInfo) {
+                 switch (platformType) {
+                         case SSDKPlatformTypeWechat:
+                                 [appInfo SSDKSetupWeChatByAppId:kWeChatAppID appSecret:kWeChatAppSecret];
+                                 break;
+                         case SSDKPlatformTypeQQ:
+                                 [appInfo SSDKSetupQQByAppId:kQQAppID appKey:kQQAppKey authType:SSDKAuthTypeBoth];
+                                 break;
+                         default:
+                                 break;
+                 }
+         }];
+#endif
 }
 
 #if defined(kXGPushAppID) && defined(kXGPushAppKey)
@@ -83,6 +131,131 @@
         } else if (notification.userInfo[ESAppRemoteNotificationKey]) {
                 [XGPush handleReceiveNotification:notification.userInfo[ESAppRemoteNotificationKey]];
         }
+}
+#endif
+
+#if defined(kShareSDKAppKey)
+- (BOOL)showShareWithData:(NSDictionary *)shareData shareStateChanged:(void (^)(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end))shareStateChanged
+{
+        if (!ESIsDictionaryWithItems(shareData)) {
+                return NO;
+        }
+        
+        // platforms: [...] 要显示的platformTypes
+        if (!ESIsArrayWithItems(shareData[@"platforms"])) {
+                return NO;
+        }
+        NSMutableArray *platforms = [NSMutableArray array];
+        for (id pf in shareData[@"platforms"]) {
+                NSInteger pfType = ESIntegerValue(pf);
+                if (pfType > 0) {
+                        [platforms addObject:@(pfType)];
+                }
+        }
+        if (!ESIsArrayWithItems(platforms)) {
+                return NO;
+        }
+        
+        NSString *type = ESStringValue(shareData[@"type"]);
+        SSDKContentType contentType;
+        if ([type isEqualToString:@"text"]) {
+                contentType = SSDKContentTypeText;
+        } else if ([type isEqualToString:@"web"]) {
+                contentType = SSDKContentTypeWebPage;
+        } else if ([type isEqualToString:@"app"]) {
+                contentType = SSDKContentTypeApp;
+        } else {
+                return NO;
+        }
+        
+        NSString *title = ESStringValue(shareData[@"title"]);
+        NSString *text = ESStringValue(shareData[@"text"]);
+        id icon = shareData[@"icon"] ?: [UIImage imageNamed:@"AppIcon60x60@3x.png"];
+        id image = shareData[@"img"];
+        NSURL *url = ESURLValue(shareData[@"url"]);
+        NSString *mailRecipient = ESStringValue(shareData[@"mail_recv"]);
+        NSString *smsRecipient = ESStringValue(shareData[@"sms_recv"]);
+        
+        NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+        [shareParams SSDKSetupShareParamsByText:text
+                                         images:image
+                                            url:url
+                                          title:title
+                                           type:SSDKContentTypeAuto];
+        for (id _pfType in platforms) {
+                SSDKPlatformType pfType = [_pfType unsignedIntegerValue];
+                if (SSDKPlatformSubTypeWechatSession == pfType ||
+                    SSDKPlatformSubTypeWechatTimeline == pfType ||
+                    SSDKPlatformSubTypeWechatFav == pfType) {
+                        [shareParams SSDKSetupWeChatParamsByText:text
+                                                           title:title
+                                                             url:url
+                                                      thumbImage:icon
+                                                           image:image
+                                                    musicFileURL:nil extInfo:nil fileData:nil emoticonData:nil
+                                                            type:contentType
+                                              forPlatformSubType:pfType];
+                } else if (SSDKPlatformSubTypeQQFriend == pfType ||
+                           SSDKPlatformSubTypeQZone == pfType) {
+                        [shareParams SSDKSetupQQParamsByText:text
+                                                       title:title
+                                                         url:url
+                                                  thumbImage:icon
+                                                       image:image
+                                                        type:(SSDKContentTypeApp == contentType ? SSDKContentTypeWebPage : contentType)
+                                          forPlatformSubType:pfType];
+                } else if (SSDKPlatformTypeMail == pfType) {
+                        NSMutableString *mailContent = [NSMutableString string];
+                        if (ESIsStringWithAnyText(text)) [mailContent appendFormat:@"%@\n", text];
+                        if (ESIsStringWithAnyText(url.absoluteString)) [mailContent appendFormat:@"%@\n", url.absoluteString];
+                        [shareParams SSDKSetupMailParamsByText:mailContent
+                                                         title:title
+                                                        images:image
+                                                   attachments:nil
+                                                    recipients:(mailRecipient ? @[mailRecipient] : nil)
+                                                  ccRecipients:nil bccRecipients:nil
+                                                          type:image ? SSDKContentTypeImage : SSDKContentTypeText];
+                } else if (SSDKPlatformTypeSMS == pfType || SSDKPlatformTypeCopy == pfType) {
+                        NSMutableString *smsContent = [NSMutableString string];
+                        if (ESIsStringWithAnyText(title)) [smsContent appendFormat:@"%@\n\n", title];
+                        if (ESIsStringWithAnyText(text)) [smsContent appendFormat:@"%@\n", text];
+                        if (ESIsStringWithAnyText(url.absoluteString)) [smsContent appendFormat:@"%@", url.absoluteString];
+                        if (SSDKPlatformTypeSMS == pfType) {
+                                [shareParams SSDKSetupSMSParamsByText:smsContent title:nil images:nil attachments:nil
+                                                           recipients:(smsRecipient ? @[smsRecipient] : nil)
+                                                                 type:SSDKContentTypeText];
+                        } else if (SSDKPlatformTypeCopy == pfType) {
+                                [shareParams SSDKSetupCopyParamsByText:smsContent
+                                                                images:nil url:nil type:SSDKContentTypeText];
+                        }
+                }
+        }
+        
+        void (^notifyShareStateChanged)(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) =
+        ^(SSDKResponseState state, SSDKPlatformType platformType, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error, BOOL end) {
+                if (shareStateChanged) {
+                        shareStateChanged(state, platformType, userData, contentEntity, error, end);
+                }
+        };
+        
+        if (platforms.count == 1) {
+                SSDKPlatformType pfType = [platforms.firstObject unsignedIntegerValue];
+                notifyShareStateChanged(SSDKResponseStateBegin, pfType, nil, nil, nil, YES);
+                [ShareSDK share:pfType
+                     parameters:shareParams
+                 onStateChanged:^(SSDKResponseState state, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error) {
+                         notifyShareStateChanged(state, pfType, userData, contentEntity, error, YES);
+                 }];
+        } else {
+                SSUIShareActionSheetController *shareController =
+                [ShareSDK showShareActionSheet:nil
+                                         items:platforms
+                                   shareParams:shareParams
+                           onShareStateChanged:notifyShareStateChanged];
+                [shareController.directSharePlatforms addObjectsFromArray:platforms];
+        }
+        
+        return YES;
 }
 #endif
 
