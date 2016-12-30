f521d383046354c7d74681f50f5b15b5b60e4df1
Elf Sundae <elf.sundae@gmail.com>
2016-12-30 22:42:49 +0800

    Delete deprecated files

--- deprecated/ACRemoteNotificationXGPushService.h
+++
@@ -1,5 +0,0 @@
-#import <AppComponents/ACRemoteNotificationServiceRegister.h>
-
-@interface ACRemoteNotificationXGPushService : NSObject <ACRemoteNotificationServiceProtocol>
-+ (void)setTags:(NSArray *)tags;
-@end
